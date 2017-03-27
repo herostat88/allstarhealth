e17d5dcaa042b56d20e4d9524f8d184196d82e4c
herostat88 <herostat88@gmail.com>
2017-03-27 16:26:32 +0300

    init

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+getPage = request({uri:'http://www.allstarhealth.com/', method:'GET', encoding:'binary'},
+    function (err, res, page) {
+        //Передаём страницу в cheerio
+        var $=cheerio.load(page);
+        //Идём по DOM-дереву обычными CSS-селекторами
+        // img_src=$('div.s9a3 > div > div > a > div > div > img').attr("src");
+        console.log($);
+    });
 
 // Exports
 
-module.exports = request
+module.exports = getPage;
